c7fb5cf48dcadf5df12f1e7af033bcf1312ef21e
ElizabethWhileyMYOB <elizabeth.whiley@myob.com>
2020-09-23 17:17:11 +1000

    Add network script

--- network.sh
+++ network.sh
@@ -8,3 +8,9 @@ docker build -t nginx-service ./nginx
 
 # create the network
 docker network create mini-network
+
+# start the flask container and add it to the network
+docker run --network=mini-network -itd --name flask-service -p 80:80 flask-service
+
+# start the nginx container
+docker run --network=mini-network -itd --name nginx-service -e 8080 nginx-service
