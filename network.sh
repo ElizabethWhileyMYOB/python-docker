f178651081ad46659054960cc43ed0cea2e94bb1
ElizabethWhileyMYOB <elizabeth.whiley@myob.com>
2020-09-30 17:34:26 +1000

    Fix nginx

--- network.sh
+++ network.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # build the flask container
-docker build -t flask-service ./flask
+docker build -t flask ./flask
 
 # build the nginx container
-docker build -t nginx-service ./nginx
+docker build -t nginx ./nginx
 
 # create the network
 docker network create mini-network
 
 # start the flask container and add it to the network
-docker run --network=mini-network -itd --name flask-service -p 80:80 flask-service
+docker run --rm --expose 8080 --network=mini-network --env APP_NAME=MyFlaskApp -itd --name flask flask
 
 # start the nginx container
-docker run --network=mini-network -itd --name nginx-service -e 8080 nginx-service
+docker run --rm -p 80:80 --network=mini-network --name nginx nginx
